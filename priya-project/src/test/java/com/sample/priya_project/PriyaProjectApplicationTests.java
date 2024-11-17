77ce8aac0537a8914e0aad22c42050a43a3aa37b
PRIYADHARSHINI-25 <83507058+PRIYADHARSHINI-25@users.noreply.github.com>
2024-11-17 17:43:34 +0530

    Update PriyaProjectApplicationTests.java

--- priya-project/src/test/java/com/sample/priya_project/PriyaProjectApplicationTests.java
+++ priya-project/src/test/java/com/sample/priya_project/PriyaProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package com.sample.priya_project;
+// package com.sample.priya_project;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PriyaProjectApplicationTests {
+// @SpringBootTest
+// class PriyaProjectApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
